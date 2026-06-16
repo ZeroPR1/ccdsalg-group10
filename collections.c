93d49466fb08be727c18e97200a0a8b683572ffb
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-16 20:15:34 +0800

    Add stack and queue operations in collections.c

--- collections.c
+++ collections.c
@@ -12,3 +12,63 @@ void push(Stack* s, Token t){
     s->data[s->top] = t;
   }
 }
+
+Token pop(Stack* s){
+  Token t;
+  t.type = TOKEN_INVALID;
+  if (s->top >= 0){
+      t =  s->data[s->top];
+      s->top =  s->top - 1;
+  }
+  return t;
+}
+
+Token peekStack(Stack* s){
+    Token t;
+    t.type = TOKEN_INVALID;
+    if (s->top >=0){
+        t = s->data[s->top];
+    }
+    return t;
+}
+
+int isStackEmpty(Stack* s){
+    int empty = 0;
+    if (s->top == -1) {
+        empty = 1;
+    }
+    return empty;
+}
+
+void initQueue(Queue* q){
+    q->front = 0;
+    q->rear = -1;
+    q->size = 0;
+}
+
+void enqueue(Queue* q, Token t){
+    if(q->size < MaxCapacity){
+      q->rear = (q->rear + 1) % MaxCapacity;
+      q->data[q->rear] = t;
+      q->size = q->size + 1;
+    }
+}
+
+Token dequeue(Queue* q){
+  Token t;
+  t.type = TOKEN_INVALID;
+  if (q->size > 0){
+      t = q->data[q->front];
+      q->front = (q->front + 1) % MaxCapacity;
+      q->size = q->size - 1;
+  }
+  return t;
+}
+
+int isQueueEmpty(Queue *q){
+  int empty = 0;
+  if (q->size == 0){
+      empty = 1;
+  }
+  return empty;
+}
