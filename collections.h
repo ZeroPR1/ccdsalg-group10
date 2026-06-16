f5545aa61ec83f3ac9b4aee2859c28c8b72d301b
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-16 16:29:41 +0800

    Update collections.h

--- collections.h
+++ collections.h
@@ -8,3 +8,28 @@ typedef struct {
   int top;
   int capacity;
 } Stack;
+
+typedef struct {
+  Token* data;
+  int front;
+  int rear;
+  int size;
+  int capacity;
+} Queue;
+
+//Stack Prototypes
+void initStack(Stack* s, int cap);
+void push(Stack* s, Token t);
+Token pop(Stack* s);
+Token peekStack(Stack* s);
+int isStackEmpty(Stack* s);
+void freeStack(Stack* s);
+
+//Queue Prototypes
+void initQueue(Queue* q, int cap);
+void enqueue(Queue* q, Token T);
+Token dequeue(Queue* q);
+int isQueueEmpty(Queue* q);
+void freeQueue(Queue* q);
+
+#endif
