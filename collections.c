aced96c58c98be25a5ead4e47b18feca8dd7db82
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-16 16:37:39 +0800

    Update collections.c

--- collections.c
+++ collections.c
@@ -2,5 +2,13 @@
 #include "collections.h"
 
 //Stack Operations
-void initStack*(Stack* s, int cap){
-s->data = (Token*)
+void initStack(Stack* s, int cap){
+  s->top = -1;
+}
+
+void push(Stack* s, Token t){
+  if (s->top < MaxCapacity - 1){
+    s->top = s->top + 1;
+    s->data[s->top] = t;
+  }
+}
