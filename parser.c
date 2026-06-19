4177e50074b95505bf42ceef9a253a8c90c32a75
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-19 12:02:11 +0800

    Add isDigitCustom function to check digit characters

--- parser.c
+++ parser.c
@@ -8,3 +8,11 @@ int isSpaceCustom(char c){ //checks if the char has whitespace
   }
   return result;
 }
+
+int isDigitCustom(char c){
+  int result = 0;
+  if (c >= '0' && c <= '9'){
+      result = 1;
+  }
+  return result;
+}
