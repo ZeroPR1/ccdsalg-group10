12a6a2ed8294443256aaf6ada487deb5135d3f39
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-19 12:01:07 +0800

    Add isSpaceCustom function to check for whitespace

--- parser.c
+++ parser.c
@@ -1,2 +1,10 @@
 #include <string.h>
 #include "parser.h"
+
+int isSpaceCustom(char c){ //checks if the char has whitespace
+  int result = 0;
+  if(c == ' ' || c == "\t" || c == '\n' || c == '\r'){
+      result = 1;
+  }
+  return result;
+}
