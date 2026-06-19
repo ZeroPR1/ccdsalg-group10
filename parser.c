79d55053d5a397c3d7c8ba23e13f7c9f571fea9d
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-19 12:07:51 +0800

    Update parser.c

--- parser.c
+++ parser.c
@@ -23,6 +23,23 @@ ErrorStatus tokenize(const char* input,  Queue* outputQueue){
     int len = strlen(input); //calculate the total number of characters
     int keepGoing = 1; //assigned variable to control the main loop
 
-    
+    while (i < len && keepGoing == 1){
+      char c = input[i];
+      if (isSpaceCustom(c)){
+        i = i + 1;
+      }
+      else if (isDigitCustom(c)){
+            int num = 0;
+      while  (i < len && isDigitCustom(input[i])){
+        num = (num * 10) + (input[i] - '0');
+        i = i + 1;
+        }
+        Token t;
+        t.type = TOKEN_OPERAND;
+        t.value = num;
+        t.symbol = '\0';
+        Enqueue(outputQueue, t);
+      }
+    }
     
 }
