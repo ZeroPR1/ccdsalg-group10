16f1ef426a0b9994602ec9afa44cca532fa73fb4
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-21 18:12:37 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include "parser.h"
+
+void printError(ErrorStatus status){
+    printf("Status: ");
+    switch (status){
+        case ERR_NONE: printf("Success\n");
+        case ERR_MISMATCH_PAREN
+    }
+}
