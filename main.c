8997a513d0befe18f0f2d3dc6ad376c8b8532391
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-06-21 18:19:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 #include <time.h>
 #include "parser.h"
 
-void printError(ErrorStatus status){
+void printError(ErrorStatus status){ //helper function to print the error messages
     printf("Status: ");
     switch (status){
-        case ERR_NONE: printf("Success\n");
-        case ERR_MISMATCH_PAREN
+        case ERR_NONE: printf("Success\n"); break;
+        case ERR_MISMATCH_PAREN: printf("Mismatched Parenthesis\n"); break;
+        case ERR_INVALID_CHAR: printf("Invalid Character\n"); break;
+        case ERR_MALFORMED: printf("Malformed expression\n"); break;
+        case ERR_DIV_ZERO: printf("Division by zero\n"); break;
+        default: printf("Error Unknown\n"); break;
     }
 }
