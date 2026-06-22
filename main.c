6b9e8eee78f23e8aeaa28e668c5c26838f4a1d44
eivannchua <eivannchua@gmail.com>
2026-06-22 18:02:07 +0800

    Added menu for user input and to run the test cases file

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "parser.h"
 
-void printError(ErrorStatus status){ //helper function to print the error messages
+// Helper function to print the status messages
+void printStatus(ErrorStatus status)
+{ 
     printf("Status: ");
-    switch (status){
+    switch (status)
+    {
         case ERR_NONE: printf("Success\n"); break;
         case ERR_MISMATCH_PAREN: printf("Mismatched Parenthesis\n"); break;
         case ERR_INVALID_CHAR: printf("Invalid Character\n"); break;
@@ -14,15 +19,17 @@ void printError(ErrorStatus status){ //helper function to print the error messag
     }
 }
 
-int main(){
-    const char* expression = "((5 + 3) * 2) ^ (1 + 1)"; //expected expression
+
+// Function that evaluates and times the arithmetic execution
+void evaluateExpression(const char *expression) 
+{
     Queue infixQueue = CreateQueue();
     Queue postfixQueue = CreateQueue();
     int finalResult = 0;
     
-    printf("Evaluating Expression: %s\n", expression); //Initalizes the queues needed for the pipeline
+    printf("Evaluating Expression: %s\n", expression); // Initializes the queues needed for the pipeline
 
-    clock_t start_time = clock(); //Starts the timer
+    clock_t start_time = clock(); // Starts the timer
 
     ErrorStatus currentStatus = tokenize(expression, &infixQueue);
     
@@ -31,19 +38,138 @@ int main(){
     }
     
     if (currentStatus == ERR_NONE) {
-        currentStatus = evaluatePostfix(&postfixQueue, &finalResult); // Runs the 3-Phase Pipeline
+        currentStatus = evaluatePostfix(&postfixQueue, &finalResult);  // Runs the 3-Phase Pipeline
     }
 
-    clock_t end_time = clock();
+    clock_t end_time = clock(); // Stops the timer
     double time_taken = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1000.0; // Stops the timer
     
     if (currentStatus == ERR_NONE) {
-        printf("Final Result: %d\n", finalResult);
+        printf("Result: %d\n", finalResult);
     } else {
-        printError(currentStatus);
+        printStatus(currentStatus);
     }
     
-    printf("Execution Time: %f ms\n", time_taken); //Prints the output
+    printf("Execution Time: %f ms\n", time_taken); // Prints the output time in milliseconds
+}
 
-    return 0;
+
+// Function that reads the test_cases.txt file
+void runFile(const char *filename) 
+{
+    // Open file in read mode
+    FILE* file = fopen(filename, "r");
+    
+    // If the file does not exist
+    if (file == NULL) 
+    {
+        printf("\nError: Could not find '%s'.\n", filename);
+    } 
+    
+    // If the file exists
+    else 
+    {
+        char line[MAX_INPUT]; // Buffer for reading the lines in test_cases.txt
+        int testCount = 1;  
+        
+        printf("\n\n======= Generated Test Cases (test_cases.txt) =======\n");
+        
+        // Read each line until the EOF
+        int lineSize = sizeof(line); 
+        while (fgets(line, lineSize, file) != NULL) 
+        {
+            // Remove the newline characters from the end of the line
+            line[strcspn(line, "\r\n")] = 0; 
+            
+            // If the line is not empty
+            if (strlen(line) > 0) 
+            { 
+                printf("\n\nTest Case #%d:\n", testCount);
+                evaluateExpression(line);  
+                testCount++; 
+            }
+        }
+        
+        fclose(file);
+    }
+}
+
+
+// Main Function
+int main() 
+{
+    char inputBuffer[MAX_INPUT]; // Holds the user's input; Can be adjusted as needed
+    int choice = 0;
+    int isActive = 1; 
+    
+    printf("\n===== CCDSALG MCO1: Arithmetic Evaluator =====");
+    
+    // Main menu
+    do 
+    {
+        printf("\n1. Input Expression\n");
+        printf("2. Run \"test_cases.txt\"\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        
+        // If user input is a valid number 
+        if (scanf("%d", &choice) == 1) 
+        {
+            // Clear input buffer
+            int c = getchar();
+            while (c != '\n' && c != EOF) 
+            { 
+                c = getchar(); 
+            } 
+            
+            // Choice 1: User input expression
+            if (choice == 1) 
+            {
+                printf("\nEnter expression: ");
+
+                // If user input is valid 
+                if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) 
+                {
+                    inputBuffer[strcspn(inputBuffer, "\r\n")] = 0; // Remove newline characters
+                    printf("\n");
+                    evaluateExpression(inputBuffer);
+                }
+            } 
+
+            // Choice 2: Run test_cases.txt
+            else if (choice == 2) 
+            {
+                runFile("test_cases.txt");
+            } 
+
+            // Choice 3: Exit
+            else if (choice == 3) 
+            {
+                printf("\nExiting...Goodbye!\n");
+                isActive = 0; 
+            } 
+
+            // If invalid input
+            else 
+            {
+                printf("\nERROR: Invalid choice!\n");
+            }
+        } 
+
+        // If user input is not a valid number
+        else 
+        {
+            printf("\nERROR: Invalid input!\n");
+            
+            // Clear input buffer
+            int c = getchar();
+            while (c != '\n' && c != EOF) 
+            { 
+                c = getchar(); 
+            }
+        }
+        
+    } while (isActive == 1);
+    
+    return 0; 
 }
